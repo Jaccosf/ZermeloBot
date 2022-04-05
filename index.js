18eb14c70fe382b5bd2b9efeb25af38fff0105e5
Jaccosf <69802108+Jaccosf@users.noreply.github.com>
2022-04-05 17:02:46 +0200

    feat(index): remove start lesson interval

--- index.js
+++ index.js
@@ -27,44 +27,6 @@ client.on("ready", () => {
 	}
 
 	client.setInterval(async () => {
-		client.database.indexes.filter(name => !(name === "knownCancelledLessons")).forEach(async (uid) => {
-
-			const accessToken = client.database.get(`${uid}`, "accessToken");
-			const school = client.database.get(`${uid}`, "school");
-
-			const ZermeloAPI = Zermelo.getAPI(school, accessToken);
-			const appointments = await ZermeloAPI.appointments.getParticipations(moment(new Date()).tz("Europe/Amsterdam").format('yyyy'), moment(new Date()).tz("Europe/Amsterdam").format('W'));
-
-      const appointment = appointments.filter(app => moment(new Date(app.start * 1000)).tz("Europe/Amsterdam").toDate() > moment(new Date()).tz("Europe/Amsterdam").toDate() && !app.cancelled)[0];
-
-			if (!appointment) return;
-
-			const difference = moment(new Date(appointment.start * 1000)).tz("Europe/Amsterdam").diff(moment(new Date()).tz("Europe/Amsterdam"));
-			const diff = moment.duration(difference);
-
-			let days = diff.days();
-			let hours = diff.hours();
-			let mins = diff.minutes();
-
-			if (!notifyTimeout.has(uid) && parseInt(days) == 0 && parseInt(hours) == 0 && parseInt(mins) <= 10) {
-
-				try {
-					client.users.cache.get(uid).send(`:warning: | Over **${client.constants.parseTimeDifference(appointment.start).days()} dagen, ${client.constants.parseTimeDifference(appointment.start).hours()} uren en ${client.constants.parseTimeDifference(appointment.start).minutes()} minuten** begint de les **${appointment.subjects.join(", ")}** (${appointment.teachers.join(", ")}) in lokaal **${appointment.locations.join(", ")}**.`);
-					client.logger.debug("Sended class notify to " + uid)
-				} catch (err) {
-					console.log(err)
-				}
-
-				notifyTimeout.add(uid);
-				setTimeout(() => {
-					notifyTimeout.delete(uid);
-				}, 800000);
-			}
-
-		})
-	}, 5000);
-
-	client.setInterval(async () => {
 		let knownCancelledLessons = client.database.get("knownCancelledLessons");
 
 		client.database.indexes.filter(name => !(name === "knownCancelledLessons")).forEach(async (uid) => {
